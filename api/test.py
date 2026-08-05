ec09aa649491833045d54afd72b11307841db6d0
mikecruz1205 <miguel.paiva.cruz3@gmail.com>
2026-08-06 00:42:33 +0100

    Refactor handler to use BaseHTTPRequestHandler

--- api/test.py
+++ api/test.py
@@ -1,5 +1,8 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Funciona!"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Funciona!".encode("utf-8"))
